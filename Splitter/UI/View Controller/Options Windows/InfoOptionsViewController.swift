30b0d129845605f79dea0aa30cac01de8d94e353
Michael Berk <michaelberkdev@gmail.com>
2020-03-07 23:22:33 -0600

    Attempts counter is now functional Renamed "next" to "Split" Prev/Split buttons now have equal width

--- Splitter/UI/View Controller/Options Windows/InfoOptionsViewController.swift
+++ Splitter/UI/View Controller/Options Windows/InfoOptionsViewController.swift
@@ -39,6 +39,7 @@ class InfoOptionsViewController: NSViewController, NSPopoverDelegate, advancedTa
 	
 	func setupDelegate() {
 		getDataFromMain()
+		attemptField.formatter = OnlyIntegerValueFormatter()
 		
 //		StartDateFormatter.date(from: startTimeLabel.stringValue)
 		
@@ -47,4 +48,26 @@ class InfoOptionsViewController: NSViewController, NSPopoverDelegate, advancedTa
 	
 	
     
+}
+
+class OnlyIntegerValueFormatter: NumberFormatter {
+
+	override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+
+		// Ability to reset your field (otherwise you can't delete the content)
+		// You can check if the field is empty later
+		if partialString.isEmpty {
+			return true
+		}
+
+		// Optional: limit input length
+		/*
+		if partialString.characters.count>3 {
+			return false
+		}
+		*/
+
+		// Actual check
+		return Int(partialString) != nil
+	}
 }
