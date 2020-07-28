c9d2b0a3f55f543b2372559810f6c0071eaf97c9
Michael Berk <moshe.berk@icloud.com>
2020-07-28 09:32:01 -0500

    Move showHideUI to UISettings.swift Move UISettings to UI folder

--- Splitter/UI/View Controller/UISettings.swift
+++ Splitter/UI/View Controller/UISettings.swift
@@ -1,8 +1,8 @@
 //
-//  ShowHideUI.swift
+//  UISettings.swift
 //  Splitter
 //
-//  Created by Michael Berk on 1/26/20.
+//  Created by Michael Berk on 1/7/20.
 //  Copyright © 2020 Michael Berk. All rights reserved.
 //
 
@@ -10,6 +10,44 @@ import Foundation
 import Cocoa
 
 extension ViewController {
+	
+	var showHideTitleBarItemText: String {
+		get {
+			if titleBarHidden {
+				return "Show Title Bar"
+			} else {
+				return "Hide Title Bar"
+			}
+		}
+	}
+	
+	///Shows or hides the title bar, depending on the current setting
+	func showHideTitleBar() {
+		if titleBarHidden {
+			view.window?.styleMask.insert(.fullSizeContentView)
+			view.window?.standardWindowButton(.closeButton)?.isHidden = true
+			view.window?.standardWindowButton(.documentIconButton)?.isHidden = true
+			view.window?.titleVisibility = .hidden
+		} else {
+			view.window?.standardWindowButton(.closeButton)?.isHidden = false
+			view.window?.standardWindowButton(.documentIconButton)?.isHidden = false
+			view.window?.titleVisibility = .visible
+
+			view.window?.styleMask.remove(.fullSizeContentView)
+			
+			
+			
+//
+		}
+		let showHideTitleBarItem = NSApp.mainMenu?.item(withIdentifier: menuIdentifiers.appearanceMenu.hideTitleBar)
+		showHideTitleBarItem?.title = showHideTitleBarItemText
+	}
+	
+	
+	@IBAction func showHideTitleBarMenuItem(_ sender: Any? ) {
+		titleBarHidden.toggle()
+		showHideTitleBar()
+	}
 	
 	///Shows or hides the UI, depending on the current setting
 	func showHideUI() {
@@ -63,3 +101,5 @@ extension ViewController {
 		}
 	}
 }
+
+//static let ShowHideUIID = "HideButtonsItem"
