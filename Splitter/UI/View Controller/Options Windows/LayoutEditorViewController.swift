a77b7ea95d9a8ea451be2b0d1efcb843d0808f53
Michael Berk <michaelberkdev@gmail.com>
2021-07-09 17:39:55 -0500

    Implemented header for components

--- Splitter/UI/View Controller/Options Windows/LayoutEditorViewController.swift
+++ Splitter/UI/View Controller/Options Windows/LayoutEditorViewController.swift
@@ -22,7 +22,9 @@ class LayoutEditorViewController: NSViewController, NSOutlineViewDelegate, NSOut
 	
 	override func viewWillAppear() {
 		super.viewWillAppear()
-		outlineView.selectRowIndexes(IndexSet([0]), byExtendingSelection: false)
+		outlineView.expandItem(nil, expandChildren: true)
+		let firstComponent = outlineView.child(0, ofItem: componentsHeaderObject)
+		outlineView.selectRowIndexes(IndexSet([outlineView.row(forItem: firstComponent)]), byExtendingSelection: false)
 		highlightAndShowOptions()
 	}
 	
@@ -32,6 +34,7 @@ class LayoutEditorViewController: NSViewController, NSOutlineViewDelegate, NSOut
 	var rows = ["Row 1", "Row 2", "Row 3"]
 	var dropType: NSPasteboard.PasteboardType = .init("public.data")
 	var runController: ViewController!
+	var draggingItem: Any?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,66 +76,25 @@ class LayoutEditorViewController: NSViewController, NSOutlineViewDelegate, NSOut
 		}
 	}
 	
-	func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
-		if item == nil {
-			return runController.mainStackView.views.count
-		}
-		return 0
-	}
-	func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
-		return false
-	}
-	
-	func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-		if item == nil {
-			return runController.mainStackView.views[index]
-		}
-		return ""
-	}
-	func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
-		if let cell = outlineView.makeView(withIdentifier: .init("textCell"), owner: self) as? NSTableCellView,
-		   let component = item as? SplitterComponent,
-		   let type = SplitterComponentType.FromType(component){
-			cell.textField?.stringValue = type.displayTitle
-			return cell
-		}
-		return nil
-	}
-	
-	func outlineView(_ outlineView: NSOutlineView, acceptDrop info: NSDraggingInfo, item: Any?, childIndex index: Int) -> Bool {
-		let pasteboard = info.draggingPasteboard
-		if let data = pasteboard.string(forType: dropType),
-		   let oldRow = Int(data),
-		   let itemToMove = outlineView.item(atRow: oldRow) {
-			var views = runController.mainStackView.views
-			views.move(fromOffsets: IndexSet([oldRow]), toOffset: index)
-			runController.mainStackView.update(runController.mainStackView, views)
-			outlineView.reloadData()
-			let indexToSelect = outlineView.childIndex(forItem: itemToMove)
-			outlineView.selectRowIndexes(IndexSet([indexToSelect]), byExtendingSelection: false)
-			return true
-		}
-		return false
-	}
 	
 	func highlightAndShowOptions() {
-		let selected = outlineView.selectedRow
-		for i in 0..<runController.mainStackView.views.count {
-			if let selectedComponent = runController.mainStackView.views[i] as? SplitterComponent {
-				if i == selected {
-					selectedComponent.isSelected = true
-					let ov = selectedComponent.optionsView!
-					scrollView.documentView = ov
-					ov.translatesAutoresizingMaskIntoConstraints = false
-					NSLayoutConstraint.activate([
-						ov.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-						ov.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: -5),
-						ov.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -5)
-					])
-				} else {
-					selectedComponent.isSelected = false
-				}
+		for view in runController.mainStackView.views {
+			let component = view as! SplitterComponent
+			let outlineRow = outlineView.row(forItem: component)
+			if outlineRow == outlineView.selectedRow {
+				component.isSelected = true
+				let ov = component.optionsView!
+				scrollView.documentView = ov
+				ov.translatesAutoresizingMaskIntoConstraints = false
+				NSLayoutConstraint.activate([
+					ov.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+					ov.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: -5),
+					ov.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -5)
+				])
+			} else {
+				component.isSelected = false
 			}
+			
 		}
 	}
 	func outlineViewSelectionDidChange(_ notification: Notification) {
@@ -148,22 +110,135 @@ class LayoutEditorViewController: NSViewController, NSOutlineViewDelegate, NSOut
 		
 		super.viewWillDisappear()
 	}
+	let componentsHeaderObject = HeaderObject("Components")
+}
+
+class HeaderObject: NSObject {
+	var string: String
+	
+	init(_ string: String) {
+		self.string = string
+	}
+	
+	override func isEqual(_ object: Any?) -> Bool {
+		if let object = object as? HeaderObject,
+		   object.string == self.string {
+			return true
+		}
+		return false
+	}
+}
+
+//MARK: - Outline View Delegate/Data Source
+extension LayoutEditorViewController {
+	
+	
+	func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+		if item == nil {
+			return 1
+		}
+		if let item = item as? HeaderObject {
+			if item == componentsHeaderObject {
+				return runController.mainStackView.views.count
+			}
+		}
+		return 0
+	}
+	func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+		if let item = item as? HeaderObject, item == componentsHeaderObject {
+			return true
+		}
+		return false
+	}
+	func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+		if item == nil {
+			return componentsHeaderObject
+		}
+		if let item = item as? HeaderObject,
+		   item == componentsHeaderObject {
+			return runController.mainStackView.views[index]
+		}
+		return ""
+	}
+	func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+		if let item = item as? HeaderObject, item == componentsHeaderObject,
+		   let cell = outlineView.makeView(withIdentifier: .init("headerCell"), owner: self) as? NSTableCellView {
+			cell.textField?.stringValue = "Components"
+			return cell
+		}
+		if let cell = outlineView.makeView(withIdentifier: .init("textCell"), owner: self) as? NSTableCellView,
+		   let component = item as? SplitterComponent,
+		   let type = SplitterComponentType.FromType(component){
+			cell.textField?.stringValue = type.displayTitle
+			return cell
+		}
+		return nil
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, shouldSelectItem item: Any) -> Bool {
+		if let item = item as? HeaderObject, item == componentsHeaderObject {
+			return false
+		}
+		return true
+	}
+	func outlineView(_ outlineView: NSOutlineView, shouldCollapseItem item: Any) -> Bool {
+		if let item = item as? HeaderObject, item == componentsHeaderObject {
+			return false
+		}
+		return true
+	}
+	func outlineView(_ outlineView: NSOutlineView, shouldShowOutlineCellForItem item: Any) -> Bool {
+		return false
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, acceptDrop info: NSDraggingInfo, item: Any?, childIndex index: Int) -> Bool {
+		if let dragItem = draggingItem {
+			if let component = dragItem as? SplitterComponent {
+				let oldIndex = runController.mainStackView.views.firstIndex(of: component)!
+				var views = runController.mainStackView.views
+				views.move(fromOffsets: IndexSet([oldIndex]), toOffset: index)
+				runController.mainStackView.update(runController.mainStackView, views)
+				outlineView.reloadData()
+				let indexToSelect = outlineView.row(forItem: component)
+				outlineView.selectRowIndexes(IndexSet([indexToSelect]), byExtendingSelection: false)
+				return true
+			}
+		}
+		return false
+	}
+	func outlineView(_ outlineView: NSOutlineView, draggingSession session: NSDraggingSession, willBeginAt screenPoint: NSPoint, forItems draggedItems: [Any]) {
+		draggingItem = draggedItems[0] as Any?
+		session.draggingPasteboard.setData(Data(), forType: dropType)
+	}
 	
 	func outlineView(_ outlineView: NSOutlineView, pasteboardWriterForItem item: Any) -> NSPasteboardWriting? {
-		let row = outlineView.row(forItem: item)
+		if let item = item as? HeaderObject, item == componentsHeaderObject {
+			return nil
+		}
 		let pbItem = NSPasteboardItem()
-		pbItem.setString(String(row), forType: dropType)
+		pbItem.setDataProvider(self, forTypes: [dropType])
 		return pbItem
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, draggingSession session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
+		self.draggingItem = nil
 	}
 	
 	func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo, proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
 		if index < 0 {
 			return .init()
 		}
-		outlineView.setDropItem(nil, dropChildIndex: index)
+		if let item = item as? HeaderObject {
+			outlineView.setDropItem(item, dropChildIndex: index)
+		}
 		return .generic
 	}
-	
+}
+extension LayoutEditorViewController: NSPasteboardItemDataProvider {
+	func pasteboard(_ pasteboard: NSPasteboard?, item: NSPasteboardItem, provideDataForType type: NSPasteboard.PasteboardType) {
+		
+		item.setString("Layout Editor Item", forType: type)
+	}
 }
 
 extension Array {
