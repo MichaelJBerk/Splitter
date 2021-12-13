b7eab51473c1c9c11d09fc66d76657a5885d0a57
Michael Berk <michaelberkdev@gmail.com>
2021-12-13 18:25:26 -0500

    Add right-click items to Status item

--- Splitter/Prefs/GeneralPrefsViewController.swift
+++ Splitter/Prefs/GeneralPrefsViewController.swift
@@ -45,12 +45,19 @@ class GeneralPrefsViewController: NSViewController, PreferencePane {
         super.viewDidLoad()
         // Do view setup here.
 		preferredContentSize = NSSize(width: self.view.frame.size.width, height: self.view.frame.size.height)
+		NotificationCenter.default.addObserver(forName: .menuBarModeChanged, object: nil, queue: nil, using: { _ in
+			self.setMenuBarToggle()
+		})
+		setMenuBarToggle()
+    }
+	
+	func setMenuBarToggle() {
 		if Settings.menuBarMode {
 			menuBarCheck.state = .on
 		} else {
 			menuBarCheck.state = .off
 		}
-    }
+	}
     
 }
 
--- Splitter/StatusBarController.swift
+++ Splitter/StatusBarController.swift
@@ -11,23 +11,36 @@ import Cocoa
 class StatusBarController: NSObject {
 	
 	var statusItem: NSStatusItem
+	var statusMenu: NSMenu
+	
+	var menuItems: [NSMenuItem] = [
+		NSMenuItem(title: "Quit", action: #selector(quitMenuItem(_:)), keyEquivalent: ""),
+		NSMenuItem(title: "Turn Off Overlay Mode", action: #selector(turnOffOverlayAction(_:)), keyEquivalent: "")
+	]
 	
 	override init() {
 		statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+		statusMenu = NSMenu(title: "PopMenu")
+		
 		super.init()
+		statusMenu.delegate = self
+		for item in menuItems {
+			item.target = self
+			statusMenu.addItem(item)
+		}
 		if let button = statusItem.button {
 			if #available(macOS 11.0, *) {
 				button.image = NSImage(systemSymbolName: "gamecontroller", accessibilityDescription: nil)!
 			}
 			button.target = self
 			button.action = #selector(clickItem(_:))
+			statusItem.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
 		}
+		
 		NotificationCenter.default.addObserver(forName: .menuBarModeChanged, object: nil, queue: nil, using: { _ in
 			self.toggleVisible()
 		})
 		Self.setMenuBarMode(Settings.menuBarMode)
-		
-		
 	}
 	
 	func toggleVisible() {
@@ -38,15 +51,34 @@ class StatusBarController: NSObject {
 		}
 	}
 	
+	@objc func quitMenuItem(_ sender: Any?) {
+		NSApp.terminate(nil)
+	}
+	@objc func turnOffOverlayAction(_ sender: Any?) {
+		Self.setMenuBarMode(false)
+	}
+	
 	@objc func clickItem(_ sender: Any?) {
-		NSApp.activate(ignoringOtherApps: true)
-		//Always 1 window - status item
-		AppDelegate.shared?.newWindowIfNone()
+		let event = NSApp.currentEvent!
+		if event.type == .rightMouseUp {
+			statusItem.menu = statusMenu
+			statusItem.button?.performClick(nil)
+		} else {
+			NSApp.activate(ignoringOtherApps: true)
+			//Always 1 window - status item
+			AppDelegate.shared?.newWindowIfNone()
+		}
 	}
 	
 	static func setMenuBarMode(_ bool: Bool) {
 		Settings.menuBarMode = bool
 		let pol: NSApplication.ActivationPolicy = bool ? .accessory : .regular
 		NSApp.setActivationPolicy(pol)
+	}
+}
+
+extension StatusBarController: NSMenuDelegate {
+	func menuDidClose(_ menu: NSMenu) {
+		statusItem.menu = nil
 	}
 }
