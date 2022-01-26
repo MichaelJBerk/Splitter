c1e88ca2278eecd1a7a092eee5a5a7802772f117
Michael Berk <michaelberkdev@gmail.com>
2022-01-25 21:33:30 -0500

    Refactor splits component options

--- Splitter/UI/View Controller/Components/SplitsComponent/SplitsComponentOptions.swift
+++ Splitter/UI/View Controller/Components/SplitsComponent/SplitsComponentOptions.swift
@@ -53,159 +53,95 @@ extension SplitsComponent {
 		}
 	
 	}
-	func differenceOptions() -> NSView {
+	
+	//MARK: - Main Options View
+	var optionsView: NSView! {
+		let d = defaultComponentOptions() as! ComponentOptionsVstack
+		let showHeaderButton = ComponentOptionsButton(checkboxWithTitle: "Show Header") {_ in
+			self.showHeader.toggle()
+		}
+		showHeaderButton.state = .init(bool: showHeader)
+		d.addArrangedSubview(showHeaderButton)
 		
-        let diffColumnIndex = self.splitsTableView.column(withIdentifier: STVColumnID.differenceColumn)
-		let sumOfBestMenuItem = NSMenuItem(title: "Sum of Best Time", action: nil, keyEquivalent: "")
-		let prevMenuItem = NSMenuItem(title: "Previous Attempt", action: nil, keyEquivalent: "")
-		let pbMenuItem = NSMenuItem(title:"Personal Best Time", action: nil, keyEquivalent: "")
+		let separatorView = NSView()
+		separatorView.wantsLayer = true
+		separatorView.layer?.backgroundColor = NSColor.separatorColor.cgColor
+		d.addArrangedSubview(separatorView)
+		NSLayoutConstraint.activate([
+			separatorView.heightAnchor.constraint(equalToConstant: 1)
+		])
+		colorOptions(stack: d)
+
+		let co = columnOptionsView
+		let superStack = SplitsOptionsView.makeView(tabs: [(d, "Options"), (co, "Columns")])
+		NSLayoutConstraint.activate([
+			superStack.widthAnchor.constraint(equalTo: d.widthAnchor),
+			co.widthAnchor.constraint(equalTo: superStack.widthAnchor),
+		])
+		return superStack
+	}
+	
+	//MARK: - Color Options
+	func colorOptions(stack: ComponentOptionsVstack) {
 		
-		let popupButton = ComponentPopUpButton(title: "", selectAction: { button in
-			switch button.indexOfSelectedItem {
-			case button.index(of: sumOfBestMenuItem):
-				self.run.setColumnComparison(.bestSegments, for: .diff)
-			case button.index(of: prevMenuItem):
-				self.run.setColumnComparison(.latest, for: .diff)
-			case button.index(of: pbMenuItem):
-				self.run.setColumnComparison(.personalBest, for: .diff)
-			default:
-				break
-			}
-			self.run.updateLayoutState()
-			
-			self.splitsTableView.reloadData(forRowIndexes: .init(0...self.splitsTableView.numberOfRows), columnIndexes: .init(integer: diffColumnIndex))
+		let tableColor = colorStack(title: "Background Color", runProperty: \.tableColor, defaultColor: .splitterTableViewColor)
+		stack.addArrangedSubview(tableColor.stack)
+		
+		let selectedColor = colorStack(title: "Selected Color", runProperty: \.selectedColor, defaultColor: .splitterRowSelected)
+		stack.addArrangedSubview(selectedColor.stack)
+		
+		let diffsLabel = NSTextField(labelWithString: "Diffs")
+		stack.addArrangedSubview(diffsLabel)
+		
+		
+		let longerColor = colorStack(title: "Longer Color", allowsOpacity: false, runProperty: \.longerColor, defaultColor: .defaultLongerColor)
+		stack.addArrangedSubview(longerColor.stack)
+		
+		let shorterColor = colorStack(title: "Shorter Color", allowsOpacity: false, runProperty: \.shorterColor, defaultColor: .defaultShorterColor)
+		stack.addArrangedSubview(shorterColor.stack)
+		
+		let bestColor = colorStack(title: "Best Color", allowsOpacity: false, runProperty: \.bestColor, defaultColor: .defaultBestColor)
+		stack.addArrangedSubview(bestColor.stack)
+		
+		NSLayoutConstraint.activate([
+			tableColor.stack.heightAnchor.constraint(equalToConstant: 30),
+			selectedColor.stack.heightAnchor.constraint(equalToConstant: 30),
+			selectedColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
+			selectedColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
+			longerColor.stack.heightAnchor.constraint(equalToConstant: 30),
+			longerColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
+			longerColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
+			shorterColor.stack.heightAnchor.constraint(equalToConstant: 30),
+			shorterColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
+			shorterColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
+			bestColor.stack.heightAnchor.constraint(equalToConstant: 30),
+			bestColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
+			bestColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor)
+		])
+	}
+	
+	func colorStack(title: String, allowsOpacity: Bool = true, runProperty: WritableKeyPath<SplitterRun, NSColor>, defaultColor: NSColor) -> (stack: NSStackView, well: SplitterColorWell) {
+		let label = NSTextField(labelWithString: title)
+		let well = SplitterColorWell(action: { well in
+			self.run[keyPath: runProperty] = well.color
 		})
-		
-		let menu = NSMenu()
-		menu.addItem(pbMenuItem)
-		menu.addItem(sumOfBestMenuItem)
-		menu.addItem(prevMenuItem)
-		popupButton.menu = menu
-		
-		switch run.getColumnComparison(for: .diff) {
-		case .bestSegments:
-			popupButton.select(sumOfBestMenuItem)
-		case .latest:
-			popupButton.select(prevMenuItem)
-		case .personalBest:
-			popupButton.select(pbMenuItem)
-		default:
-			break
-		}
-    
-		let compareLabel = NSTextField(labelWithString: "Compare To")
-		let compareStack = NSStackView(views: [compareLabel, popupButton])
-        compareStack.orientation = .horizontal
-        let timeOptions = segmentSplitOptions(for: splitsTableView.tableColumns[diffColumnIndex]) as! NSStackView
-        let stack = NSStackView(views: [compareStack, timeOptions])
-        NSLayoutConstraint.activate([
-            timeOptions.views[1].leadingAnchor.constraint(equalTo: popupButton.leadingAnchor)
-        ])
-        stack.orientation = .vertical
-        stack.alignment = .leading
-        
-		return stack
-		
-	}
-    func segmentSplitOptions(for column: NSTableColumn) -> NSView {
-        let timeOptionMenu = NSMenu()
-        let segmentTimeOption = NSMenuItem(title: "Segment Time", action: nil, keyEquivalent: "")
-        let splitTimeOption = NSMenuItem(title: "Split Time", action: nil, keyEquivalent: "")
-        timeOptionMenu.addItem(segmentTimeOption)
-        timeOptionMenu.addItem(splitTimeOption)
-        var runColumn: SplitterRun.TimeColumn?
-        switch column.identifier {
-        case STVColumnID.differenceColumn:
-            runColumn = .diff
-        case STVColumnID.currentSplitColumn:
-            runColumn = .time
-        case STVColumnID.bestSplitColumn:
-            runColumn = .pb
-        case STVColumnID.previousSplitColumn:
-            runColumn = .previous
-        default:
-            break
-        }
-        
-        let timeOptionButton = ComponentPopUpButton(title: "", selectAction: { button in
-            let h = button.indexOfSelectedItem
-            if let menuItem = button.menu?.items[h], let runColumn = runColumn {
-                switch menuItem {
-                case segmentTimeOption:
-                    if runColumn == .time {
-                        self.run.undoManager?.beginUndoGrouping()
-                        self.run.setUpdateWith(.segmentTime, for: runColumn)
-                        self.run.setStartWith(.comparsionSegmentTime, for: runColumn)
-                        self.run.undoManager?.endUndoGrouping()
-                    } else if runColumn == .diff {
-                        self.run.setUpdateWith(.segmentDelta, for: runColumn)
-                    } else {
-                        self.run.setStartWith(.comparsionSegmentTime, for: runColumn)
-                    }
-                case splitTimeOption:
-                    if runColumn == .time {
-                        self.run.undoManager?.beginUndoGrouping()
-                        self.run.setUpdateWith(.splitTime, for: runColumn)
-                        self.run.setStartWith(.comparisonTime, for: runColumn)
-                        self.run.undoManager?.endUndoGrouping()
-                    } else if runColumn == .diff {
-                        self.run.setUpdateWith(.delta, for: runColumn)
-                    } else {
-                        self.run.setStartWith(.comparisonTime, for: runColumn)
-                    }
-                default:
-                    break
-                }
-            }
-        })
-        timeOptionButton.menu = timeOptionMenu
-        if let runColumn = runColumn {
-            if runColumn == .time || runColumn == .diff {
-                let currentOption = run.getUpdateWith(for: runColumn)
-                switch currentOption {
-                case .segmentTime, .segmentDelta, .segmentDeltaWithFallback:
-                    timeOptionButton.select(segmentTimeOption)
-                case .splitTime, .delta, .deltaWithFallback:
-                    timeOptionButton.select(splitTimeOption)
-                default:
-                    break
-                }
-            } else {
-                let currentOption = run.getStartWith(for: runColumn)
-                switch currentOption {
-                case .comparsionSegmentTime:
-                    timeOptionButton.select(segmentTimeOption)
-                case .comparisonTime:
-                    timeOptionButton.select(splitTimeOption)
-                default:
-                    break
-                }
-            }
-        }
-        
-        let valueLabel = NSTextField(labelWithString: "Value")
-        let valueStack = NSStackView(views: [valueLabel, timeOptionButton])
-        
-        let helpButton = helpButton()
-        let helpText = """
-        Split Time: Total time since the beginning of the run, at the time of the split
-        
-        Segment Time: The duration of the segment
-        """
-        helpButton.helpString = helpText
-        valueStack.addArrangedSubview(helpButton)
-        
-        valueStack.orientation = .horizontal
-        return valueStack
-    }
-	
-	func options(for column: NSTableColumn) -> NSView {
-		if column.identifier == STVColumnID.differenceColumn {
-			return differenceOptions()
-		}
-		return segmentSplitOptions(for: column)
+		well.allowsOpacity = allowsOpacity
+		well.color = self.run[keyPath: runProperty]
+		let resetButton = ComponentOptionsButton(title: "Reset", clickAction: { _ in
+			well.color = defaultColor
+			well.colorWellAction(well)
+		})
+		let stack = NSStackView(views: [label, well, resetButton])
+		stack.orientation = .horizontal
+		NotificationCenter.default.addObserver(forName: .runColorChanged, object: run, queue: nil, using: { notification in
+			let run = notification.object as! SplitterRun
+			let color = run[keyPath: runProperty]
+			well.color = color
+		})
+		return (stack, well)
 	}
 	
+	//MARK: - Column Options View
 	var columnOptionsView: NSView {
 		let stack = ComponentOptionsVstack(views: [])
 		stack.wantsLayer = true
@@ -262,81 +198,157 @@ extension SplitsComponent {
 		return stack
 	}
 	
-	func colorStack(title: String, allowsOpacity: Bool = true, runProperty: WritableKeyPath<SplitterRun, NSColor>, defaultColor: NSColor) -> (stack: NSStackView, well: SplitterColorWell) {
-		let label = NSTextField(labelWithString: title)
-		let well = SplitterColorWell(action: { well in
-			self.run[keyPath: runProperty] = well.color
+	func differenceOptions() -> NSView {
+		
+		let diffColumnIndex = self.splitsTableView.column(withIdentifier: STVColumnID.differenceColumn)
+		let sumOfBestMenuItem = NSMenuItem(title: "Sum of Best Time", action: nil, keyEquivalent: "")
+		let prevMenuItem = NSMenuItem(title: "Previous Attempt", action: nil, keyEquivalent: "")
+		let pbMenuItem = NSMenuItem(title:"Personal Best Time", action: nil, keyEquivalent: "")
+		
+		let popupButton = ComponentPopUpButton(title: "", selectAction: { button in
+			switch button.indexOfSelectedItem {
+			case button.index(of: sumOfBestMenuItem):
+				self.run.setColumnComparison(.bestSegments, for: .diff)
+			case button.index(of: prevMenuItem):
+				self.run.setColumnComparison(.latest, for: .diff)
+			case button.index(of: pbMenuItem):
+				self.run.setColumnComparison(.personalBest, for: .diff)
+			default:
+				break
+			}
+			self.run.updateLayoutState()
+			
+			self.splitsTableView.reloadData(forRowIndexes: .init(0...self.splitsTableView.numberOfRows), columnIndexes: .init(integer: diffColumnIndex))
 		})
-		well.allowsOpacity = allowsOpacity
-		well.color = self.run[keyPath: runProperty]
-		let resetButton = ComponentOptionsButton(title: "Reset", clickAction: { _ in
-			well.color = defaultColor
-			well.colorWellAction(well)
-		})
-		let stack = NSStackView(views: [label, well, resetButton])
-		stack.orientation = .horizontal
-		NotificationCenter.default.addObserver(forName: .runColorChanged, object: run, queue: nil, using: { notification in
-			let run = notification.object as! SplitterRun
-			let color = run[keyPath: runProperty]
-			well.color = color
-		})
-		return (stack, well)
+		
+		let menu = NSMenu()
+		menu.addItem(pbMenuItem)
+		menu.addItem(sumOfBestMenuItem)
+		menu.addItem(prevMenuItem)
+		popupButton.menu = menu
+		
+		switch run.getColumnComparison(for: .diff) {
+		case .bestSegments:
+			popupButton.select(sumOfBestMenuItem)
+		case .latest:
+			popupButton.select(prevMenuItem)
+		case .personalBest:
+			popupButton.select(pbMenuItem)
+		default:
+			break
+		}
+	
+		let compareLabel = NSTextField(labelWithString: "Compare To")
+		let compareStack = NSStackView(views: [compareLabel, popupButton])
+		compareStack.orientation = .horizontal
+		let timeOptions = segmentSplitOptions(for: splitsTableView.tableColumns[diffColumnIndex]) as! NSStackView
+		let stack = NSStackView(views: [compareStack, timeOptions])
+		NSLayoutConstraint.activate([
+			timeOptions.views[1].leadingAnchor.constraint(equalTo: popupButton.leadingAnchor)
+		])
+		stack.orientation = .vertical
+		stack.alignment = .leading
+		
+		return stack
+		
 	}
 	
-	var optionsView: NSView! {
-		let d = defaultComponentOptions() as! ComponentOptionsVstack
-		let showHeaderButton = ComponentOptionsButton(checkboxWithTitle: "Show Header") {_ in
-			self.showHeader.toggle()
+	func segmentSplitOptions(for column: NSTableColumn) -> NSView {
+		let timeOptionMenu = NSMenu()
+		let segmentTimeOption = NSMenuItem(title: "Segment Time", action: nil, keyEquivalent: "")
+		let splitTimeOption = NSMenuItem(title: "Split Time", action: nil, keyEquivalent: "")
+		timeOptionMenu.addItem(segmentTimeOption)
+		timeOptionMenu.addItem(splitTimeOption)
+		var runColumn: SplitterRun.TimeColumn?
+		switch column.identifier {
+		case STVColumnID.differenceColumn:
+			runColumn = .diff
+		case STVColumnID.currentSplitColumn:
+			runColumn = .time
+		case STVColumnID.bestSplitColumn:
+			runColumn = .pb
+		case STVColumnID.previousSplitColumn:
+			runColumn = .previous
+		default:
+			break
 		}
-		showHeaderButton.state = .init(bool: showHeader)
-		d.addArrangedSubview(showHeaderButton)
 		
-		let separatorView = NSView()
-		separatorView.wantsLayer = true
-		separatorView.layer?.backgroundColor = NSColor.separatorColor.cgColor
-		d.addArrangedSubview(separatorView)
-		NSLayoutConstraint.activate([
-			separatorView.heightAnchor.constraint(equalToConstant: 1)
-		])
+		let timeOptionButton = ComponentPopUpButton(title: "", selectAction: { button in
+			let h = button.indexOfSelectedItem
+			if let menuItem = button.menu?.items[h], let runColumn = runColumn {
+				switch menuItem {
+				case segmentTimeOption:
+					if runColumn == .time {
+						self.run.undoManager?.beginUndoGrouping()
+						self.run.setUpdateWith(.segmentTime, for: runColumn)
+						self.run.setStartWith(.comparsionSegmentTime, for: runColumn)
+						self.run.undoManager?.endUndoGrouping()
+					} else if runColumn == .diff {
+						self.run.setUpdateWith(.segmentDelta, for: runColumn)
+					} else {
+						self.run.setStartWith(.comparsionSegmentTime, for: runColumn)
+					}
+				case splitTimeOption:
+					if runColumn == .time {
+						self.run.undoManager?.beginUndoGrouping()
+						self.run.setUpdateWith(.splitTime, for: runColumn)
+						self.run.setStartWith(.comparisonTime, for: runColumn)
+						self.run.undoManager?.endUndoGrouping()
+					} else if runColumn == .diff {
+						self.run.setUpdateWith(.delta, for: runColumn)
+					} else {
+						self.run.setStartWith(.comparisonTime, for: runColumn)
+					}
+				default:
+					break
+				}
+			}
+		})
+		timeOptionButton.menu = timeOptionMenu
+		if let runColumn = runColumn {
+			if runColumn == .time || runColumn == .diff {
+				let currentOption = run.getUpdateWith(for: runColumn)
+				switch currentOption {
+				case .segmentTime, .segmentDelta, .segmentDeltaWithFallback:
+					timeOptionButton.select(segmentTimeOption)
+				case .splitTime, .delta, .deltaWithFallback:
+					timeOptionButton.select(splitTimeOption)
+				default:
+					break
+				}
+			} else {
+				let currentOption = run.getStartWith(for: runColumn)
+				switch currentOption {
+				case .comparsionSegmentTime:
+					timeOptionButton.select(segmentTimeOption)
+				case .comparisonTime:
+					timeOptionButton.select(splitTimeOption)
+				default:
+					break
+				}
+			}
+		}
 		
-		let tableColor = colorStack(title: "Background Color", runProperty: \.tableColor, defaultColor: .splitterTableViewColor)
-		d.addArrangedSubview(tableColor.stack)
+		let valueLabel = NSTextField(labelWithString: "Value")
+		let valueStack = NSStackView(views: [valueLabel, timeOptionButton])
 		
-		let selectedColor = colorStack(title: "Selected Color", runProperty: \.selectedColor, defaultColor: .splitterRowSelected)
-		d.addArrangedSubview(selectedColor.stack)
+		let helpButton = helpButton()
+		let helpText = """
+		Split Time: Total time since the beginning of the run, at the time of the split
 		
-		let diffsLabel = NSTextField(labelWithString: "Diffs")
-		d.addArrangedSubview(diffsLabel)
+		Segment Time: The duration of the segment
+		"""
+		helpButton.helpString = helpText
+		valueStack.addArrangedSubview(helpButton)
 		
-		
-		let longerColor = colorStack(title: "Longer Color", allowsOpacity: false, runProperty: \.longerColor, defaultColor: .defaultLongerColor)
-		d.addArrangedSubview(longerColor.stack)
-		
-		let shorterColor = colorStack(title: "Shorter Color", allowsOpacity: false, runProperty: \.shorterColor, defaultColor: .defaultShorterColor)
-		d.addArrangedSubview(shorterColor.stack)
-		
-		let bestColor = colorStack(title: "Best Color", allowsOpacity: false, runProperty: \.bestColor, defaultColor: .defaultBestColor)
-		d.addArrangedSubview(bestColor.stack)
-
-		let co = columnOptionsView
-		let superStack = SplitsOptionsView.makeView(tabs: [(d, "Options"), (co, "Columns")])
-		NSLayoutConstraint.activate([
-			superStack.widthAnchor.constraint(equalTo: d.widthAnchor),
-			co.widthAnchor.constraint(equalTo: superStack.widthAnchor),
-			tableColor.stack.heightAnchor.constraint(equalToConstant: 30),
-			selectedColor.stack.heightAnchor.constraint(equalToConstant: 30),
-			selectedColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
-			selectedColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
-			longerColor.stack.heightAnchor.constraint(equalToConstant: 30),
-			longerColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
-			longerColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
-			shorterColor.stack.heightAnchor.constraint(equalToConstant: 30),
-			shorterColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
-			shorterColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor),
-			bestColor.stack.heightAnchor.constraint(equalToConstant: 30),
-			bestColor.well.widthAnchor.constraint(equalTo: tableColor.well.widthAnchor),
-			bestColor.well.leadingAnchor.constraint(equalTo: tableColor.well.leadingAnchor)
-		])
-		return superStack
+		valueStack.orientation = .horizontal
+		return valueStack
+	}
+	
+	func options(for column: NSTableColumn) -> NSView {
+		if column.identifier == STVColumnID.differenceColumn {
+			return differenceOptions()
+		}
+		return segmentSplitOptions(for: column)
 	}
 }
