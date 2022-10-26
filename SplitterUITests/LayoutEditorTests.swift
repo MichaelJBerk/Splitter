08720629600d6d5ebbd54d8b62a167ee4ee45322
Michael Berk <michaelberkdev@gmail.com>
2022-10-26 14:24:46 -0400

    Updated UI Tests

--- SplitterUITests/LayoutEditorTests.swift
+++ SplitterUITests/LayoutEditorTests.swift
@@ -11,11 +11,7 @@ import XCTest
 final class LayoutEditorTests: XCTestCase {
 	
 	var app: XCUIApplication!
-//
-//	override func setUp() {
-//
-//
-//	}
+
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -67,5 +63,41 @@ final class LayoutEditorTests: XCTestCase {
 		setColors()
 		
 	}
+	
+	func testAddRemoveColumns() {
+		let app = XCUIApplication()
+		let untitledWindow = app.windows["LongList.split"]
+		untitledWindow.buttons["Edit Components"].click()
+		let popoversQuery = untitledWindow.popovers
+		popoversQuery.outlines["Component List"].staticTexts["Splits Settings"].click()
+		
+		popoversQuery/*@START_MENU_TOKEN@*/.radioButtons["Columns"]/*[[".radioGroups.radioButtons[\"Columns\"]",".radioButtons[\"Columns\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery.buttons["add"].click()
+		popoversQuery.buttons["remove"].click()
+	}
+	
+	func testEditColumns() {
+		
+		let longlistSplitWindow = XCUIApplication().windows["LongList.split"]
+		longlistSplitWindow.buttons["Edit Components"].click()
+		
+		let popoversQuery = longlistSplitWindow.popovers
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines["Component List"].staticTexts["Splits Settings"]/*[[".scrollViews.outlines[\"Component List\"]",".outlineRows",".cells",".staticTexts[\"Splits\"]",".staticTexts[\"Splits Settings\"]",".outlines[\"Component List\"]"],[[[-1,5,1],[-1,0,1]],[[-1,4],[-1,3],[-1,2,3],[-1,1,2]],[[-1,4],[-1,3],[-1,2,3]],[[-1,4],[-1,3]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.radioButtons["Columns"]/*[[".radioGroups.radioButtons[\"Columns\"]",".radioButtons[\"Columns\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery.children(matching: .scrollView).element(boundBy: 1).outlines.children(matching: .outlineRow).element(boundBy: 3)/*@START_MENU_TOKEN@*/.disclosureTriangles["NSOutlineViewDisclosureButtonKey"]/*[[".cells.disclosureTriangles[\"NSOutlineViewDisclosureButtonKey\"]",".disclosureTriangles[\"NSOutlineViewDisclosureButtonKey\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.popUpButtons["Empty"]/*[[".scrollViews.outlines",".outlineRows",".cells.popUpButtons[\"Empty\"]",".popUpButtons[\"Empty\"]",".outlines"],[[[-1,4,1],[-1,0,1]],[[-1,3],[-1,2],[-1,1,2]],[[-1,3],[-1,2]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.menuItems["Comparison Segment Time"]/*[[".scrollViews.outlines",".outlineRows",".cells",".popUpButtons[\"Empty\"]",".menus.menuItems[\"Comparison Segment Time\"]",".menuItems[\"Comparison Segment Time\"]",".outlines"],[[[-1,6,1],[-1,0,1]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3],[-1,1,2]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3]],[[-1,5],[-1,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery.outlines.popUpButtons["Delta"].click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.menuItems["Segment Time"]/*[[".scrollViews.outlines",".outlineRows",".cells",".popUpButtons[\"Delta\"]",".menus.menuItems[\"Segment Time\"]",".menuItems[\"Segment Time\"]",".outlines"],[[[-1,6,1],[-1,0,1]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3],[-1,1,2]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3]],[[-1,5],[-1,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.popUpButtons["Contextual"]/*[[".scrollViews.outlines",".outlineRows",".cells.popUpButtons[\"Contextual\"]",".popUpButtons[\"Contextual\"]",".outlines"],[[[-1,4,1],[-1,0,1]],[[-1,3],[-1,2],[-1,1,2]],[[-1,3],[-1,2]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.menuItems["On Ending Segment"]/*[[".scrollViews.outlines",".outlineRows",".cells",".popUpButtons[\"Contextual\"]",".menus.menuItems[\"On Ending Segment\"]",".menuItems[\"On Ending Segment\"]",".outlines"],[[[-1,6,1],[-1,0,1]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3],[-1,1,2]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3]],[[-1,5],[-1,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.popUpButtons["Default (Personal Best)"]/*[[".scrollViews.outlines",".outlineRows",".cells.popUpButtons[\"Default (Personal Best)\"]",".popUpButtons[\"Default (Personal Best)\"]",".outlines"],[[[-1,4,1],[-1,0,1]],[[-1,3],[-1,2],[-1,1,2]],[[-1,3],[-1,2]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		popoversQuery/*@START_MENU_TOKEN@*/.outlines.menuItems["Balanced PB"]/*[[".scrollViews.outlines",".outlineRows",".cells",".popUpButtons[\"Default (Personal Best)\"]",".menus.menuItems[\"Balanced PB\"]",".menuItems[\"Balanced PB\"]",".outlines"],[[[-1,6,1],[-1,0,1]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3],[-1,1,2]],[[-1,5],[-1,4],[-1,3,4],[-1,2,3]],[[-1,5],[-1,4],[-1,3,4]],[[-1,5],[-1,4]]],[0,0]]@END_MENU_TOKEN@*/.click()
+		
+		longlistSplitWindow.click()
+		longlistSplitWindow.buttons["Start"].click()
+		sleep(3)
+	}
+	
 	
 }
