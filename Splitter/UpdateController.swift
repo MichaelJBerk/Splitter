63bb61339dd0f9663eeabb73102214b99ed387f4
Michael Berk <michaelberkdev@gmail.com>
2023-05-22 19:20:18 -0400

    Remove BasicUpdater calls

--- Splitter/UpdateController.swift
+++ Splitter/UpdateController.swift
@@ -13,27 +13,9 @@ import BasicUpdater
 class UpdateController:NSObject {
 	static let shared = UpdateController()
 	
-	let updater = Updater(projectURL: URL(string: "https://github.com/michaeljberk/Splitter"), shouldUpdateTo: { release in
-		let regexStr = #"\d*$"#
-		let tag = release.tagName
-		let range = tag.range(of: regexStr, options: .regularExpression)!
-		let newBuildStr = String(tag[range])
-		let newBuildNum = Int(newBuildStr)!
-		let currentBuildStr = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
-		let currentBuildNum = Int(currentBuildStr)!
-		return newBuildNum > currentBuildNum
-	})
-	
 	@objc func checkForUpdates() {
-		updater.checkForUpdates()
 	}
 	
 	func addUpdateCommand() {
-		let settingsItem = NSApp.mainMenu!.item(withIdentifier: menuIdentifiers.appMenu.settingsMenuItem)!
-		let appMenu = NSApp.mainMenu!.items[0].submenu!
-		let settingsIndex = appMenu.index(of: settingsItem)
-		let updateItem = NSMenuItem(title: "Check for Updates...", action: #selector(checkForUpdates), keyEquivalent: "")
-		updateItem.target = self
-		appMenu.insertItem(updateItem, at: settingsIndex + 1)
 	}
 }
