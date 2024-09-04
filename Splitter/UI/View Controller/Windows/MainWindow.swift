bf20a06eca81d7d42ee33cb3ec2beb6ff49f91e7
Michael Berk <michaelberkdev@gmail.com>
2024-09-03 21:09:16 -0400

    In overlay mode, always move window to the front when changing spaces

--- Splitter/UI/View Controller/Windows/MainWindow.swift
+++ Splitter/UI/View Controller/Windows/MainWindow.swift
@@ -11,9 +11,25 @@ import Cocoa
 ///This class is needed for the Hotkeys to work. I don't remeber why at the moment.
 class MainWindow: NSWindow {
 	
+	var observer: Any!
+	override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+		super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+		observer = NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.activeSpaceDidChangeNotification, object: nil, queue: nil, using: { _ in
+			if Settings.menuBarMode {
+				self.orderFront(self)
+			}
+		})
+	}
+	
 	override func setTitleWithRepresentedFilename(_ filename: String) {
 		super.setTitleWithRepresentedFilename(filename)
 		
+	}
+	
+	//need to override `close` and remove the observer or the window will reappear after changing spaces in Overlay Mode
+	override func close() {
+		super.close()
+		NSWorkspace.shared.notificationCenter.removeObserver(observer!, name: NSWorkspace.activeSpaceDidChangeNotification, object: nil)
 	}
 }
 
